ec979e7d1af29625bf9259e605de099be32b910d
niktekusho <nicola.dalmaso@outlook.com>
2019-04-08 10:00:58 +0200

    Implement tagged releases on Travis CI

--- docker_push.sh
+++ docker_push.sh
@@ -8,5 +8,14 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 # Previous build step ensures the image is working
 docker build -t nikgatto/my-dockerized-debian .
 
+# If the Travis tag env var is undefined then use latest
+if [ -z "${TRAVIS_TAG:-}" ]; then
+	PUBLISH_TAG="latest"
+else
+	PUBLISH_TAG=${TRAVIS_TAG}
+fi
+
+echo "Publish Tag is: ${PUBLISH_TAG}"
+
 # Finally push the image to the Docker Hub
-docker push nikgatto/my-dockerized-debian:latest
+docker push nikgatto/my-dockerized-debian:"${PUBLISH_TAG}"
